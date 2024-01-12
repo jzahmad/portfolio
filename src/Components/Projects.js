df99b7c2768acf0738bf1022212f243cca6dee86
Jazib Ahmad <jz346475@dal.ca>
2024-01-12 19:06:49 -0400

    Update

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -26,7 +26,7 @@ function Projects() {
       <ProjectCard
           title="Cities Explorer"
           technologies="Kotlin, XML"
-          description="Developed an application where user can explore differnet cities based on Continents."
+          description="Developed an application where user can explore different cities based on Continents."
           link="https://github.com/jzahmad/CitiesExplorer.git"
         />
 
@@ -39,25 +39,25 @@ function Projects() {
         <ProjectCard
           title="Blogging Website for Patients"
           technologies="HTML, CSS, JS, JQUERY, Bootstrap, MongoDB, NodeJS"
-          description="Collaborated with a diverse team of 4 students to build a website helping patients write and publish their blogs. Identified assignment requirements, then completed the analysis, design, implementation, and testing. Successfully communicated the scope of the project by writing clear, accurate technical specifications and presenting the project summary."
+          description="Collaborated with a diverse team of 4 students to build a website helping patients write, edit and publish their blogs."
           link="https://github.com/jzahmad/BforP"
         />
         <ProjectCard
           title="ROCK-PAPER-SCISSOR game"
           technologies="C, Unix, Socket Programming"
-          description="Developed a client and server side where the client can play ROCK-PAPER-SCISSOR with the server. Developed the program with great efficiency and documentation."
+          description="Developed a client and server side where the client can play ROCK-PAPER-SCISSOR with the server. Developed the program front-end and backend part"
           link="https://github.com/jzahmad/RPS-unix"
         />
         <ProjectCard
           title="Task Management Application (Trello)"
           technologies="ReactJS, SQL, Springboot, Git, CSS"
-          description="Developed a Trello Clone using APIs, Front-end, and backend languages with a group of 5 classmates including em using software engineering principles. Worked specifically on the front end using ReactJS and helped in developing database entity diagrams. Also worked on the backend for development using Springboot."
+          description="Developed a Trello Clone using APIs, Front-end, and backend languages with a group of 5 classmates including em using software engineering principles. "
           link="https://github.com/jzahmad/T-Clone"
         />
         <ProjectCard
           title="Line Follower & Obstacle Avoider"
-          technologies="Java, Aseba"
-          description="Developed a robotics program where a robot can follow a line and avoid all the obstacles that come in the way.I Made a report explaining all the codes and our future work."
+          technologies="Aseba"
+          description="Developed a robotics program where a robot can follow a line and avoid all the obstacles that come in the way."
           link="https://github.com/jzahmad/LineF-OjecA"
         />
 
