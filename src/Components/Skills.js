db1b94541869963873ce936807c73462ad1bbc96
Jazib Ahmad <jz346475@dal.ca>
2024-01-12 17:58:51 -0400

    Update

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import { FaReact, FaJava, FaDatabase, FaAndroid, FaPhp, FaCloud, FaUnity, FaSpotify } from 'react-icons/fa';
 import { SiKotlin } from 'react-icons/si';
-
 const SkillsScreen = () => {
   const iconSize = 40; // Set the desired size for the icons
   return (
-    <div style={{marginLeft:"20px", marginTop:"20px"}}>
-        <div style={{ marginLeft: '150px', fontSize: '28px', fontWeight: 'bold'}}>
-            <a>My Skills </a>
-            <a style={{marginLeft:"580px"}}>My Experience</a>
-        </div>
-        <div style={{ display: 'flex', justifyContent: 'left' }}>
+    <div style={{margin: "0 auto" ,textAlign:"center"}}>
+        <a style={{ marginTop:"20px", marginLeft:"100px",fontSize: '28px', fontWeight: 'bold'}}>My Skills </a>
+        <a style={{ marginTop:"20px", marginLeft:"550px",fontSize: '28px', fontWeight: 'bold'}}>Experience </a>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
             <div style={{ marginRight: '50px' }}>
-            <div style={{ marginBottom: '20px', textAlign: 'center' }}>
+                <div style={{ marginTop: '10px',marginBottom: '20px', textAlign: 'center' }}>
                 <FaReact size={iconSize} />
                 <div style={{ marginTop: '10px' }}>React</div>
             </div>
@@ -37,8 +34,8 @@ const SkillsScreen = () => {
                 <div style={{ marginTop: '10px' }}>Unity</div>
             </div>
         </div>
-        <div style={{marginLeft:"200px"}}>
-            <div style={{ marginBottom: '20px', textAlign: 'center' }}>
+        <div style={{marginLeft:"200px" }}>
+            <div style={{ marginTop:'10px',marginBottom: '20px', textAlign: 'center' }}>
                 <FaAndroid size={iconSize} />
                 <div style={{ marginTop: '10px' }}>Android Studio</div>
             </div>
@@ -63,8 +60,8 @@ const SkillsScreen = () => {
                 <div style={{ marginTop: '10px' }}>Android Development</div>
             </div>
         </div>
-        <div style={{ marginLeft: "300px", fontFamily: "Arial, sans-serif" }}>
-            <div style={{ marginTop:"10px" ,borderBottom: "2px solid #333", paddingBottom: "10px", marginBottom: "20px" }}>
+        <div style={{ marginLeft: "250px"}}>
+            <div style={{ marginTop:"25px" ,borderBottom: "2px solid #333", paddingBottom: "10px", marginBottom: "20px" }}>
                 <a style={{ fontSize: "20px", color: "#007BFF", textDecoration: "none", fontWeight: "bold" }}>Web Developer</a>
                 <div style={{ marginLeft: "30px", color: "#6C757D" }}>
                     <p>Saint Mary's University</p>
@@ -80,9 +77,9 @@ const SkillsScreen = () => {
                     </div>
                 </div>
 
-                <a style={{marginTop:"10px", marginLeft:"150px", fontSize: '28px', fontWeight: 'bold'}}>Education</a>
+                <a style={{marginTop:"10px", marginLeft:"90px", fontSize: '28px', fontWeight: 'bold'}}>Education</a>
 
-                <div style={{ marginTop:"10px" ,borderBottom: "2px solid #333", paddingBottom: "10px", marginBottom: "20px" }}>
+                <div style={{ marginTop:"25px" ,borderBottom: "2px solid #333", paddingBottom: "10px", marginBottom: "20px" }}>
                     <a style={{ fontSize: "20px", color: "#007BFF", textDecoration: "none", fontWeight: "bold" }}>Dalhousie University</a>
                     <div style={{ marginLeft: "30px", color: "#6C757D" }}>
                         <p>Bachelor Of Science in Computer Science</p>
